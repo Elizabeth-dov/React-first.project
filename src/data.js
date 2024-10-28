31977415efb66b93bbe17902e93813b4739042a9
Elizabeth Dovhaliuk <142344859+Elizabeth-dov@users.noreply.github.com>
2024-10-28 23:33:29 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,55 +1,55 @@
 
 export const photos = [
-    { id: 1, url: "../public/portfolio-1.jpg"},
-    { id: 2, url: "../public/portfolio-2.jpg"},
-    { id: 3, url: "../public/portfolio-3.jpg"},
+    { id: 1, url: "/portfolio-1.jpg"},
+    { id: 2, url: "/portfolio-2.jpg"},
+    { id: 3, url: "/portfolio-3.jpg"},
    ];
 
 export const photoss = [
-    { id: 1, url: "../public/photoss-1.jpg"},
-    { id: 2, url: "../public/photoss-2.jpg"},
-    { id: 3, url: "../public/photoss-3.jpg"},
+    { id: 1, url: "/photoss-1.jpg"},
+    { id: 2, url: "/photoss-2.jpg"},
+    { id: 3, url: "/photoss-3.jpg"},
    ];
 export const photosMore = [
     {  id: 1,
-       url: "../public/miasto.jpg",
+       url: "/miasto.jpg",
        title: "Sesja w mieście",
        description: "Sesja w mieście to świetny sposób na uchwycenie energii i rytmu życia! W miejskim krajobrazie znajdziemy różnorodne tła – od klimatycznych uliczek, przez nowoczesne budynki, po urokliwe kawiarnie. Każda sceneria może opowiedzieć inną historię, a światła miasta dodają zdjęciom wyjątkowego charakteru, zwłaszcza o zmierzchu. To propozycja dla tych, którzy lubią miejski styl i chcą zatrzymać w kadrze autentyczne emocje w dynamicznym otoczeniu."
     },
     {   id: 2, 
-        url: "../public/przyroda.jpg",
+        url: "/przyroda.jpg",
         title: "Sesja na przyrodzie",
         description: "Sesja w mieście to świetny sposób na uchwycenie energii i rytmu życia! W miejskim krajobrazie znajdziemy różnorodne tła – od klimatycznych uliczek, przez nowoczesne budynki, po urokliwe kawiarnie. Każda sceneria może opowiedzieć inną historię, a światła miasta dodają zdjęciom wyjątkowego charakteru, zwłaszcza o zmierzchu. To propozycja dla tych, którzy lubią miejski styl i chcą zatrzymać w kadrze autentyczne emocje w dynamicznym otoczeniu."
     },
     {   id: 3,
-        url: "../public/studio.jpg",
+        url: "/studio.jpg",
         title: "Sesja w studio",
         description: "Sesja w mieście to świetny sposób na uchwycenie energii i rytmu życia! W miejskim krajobrazie znajdziemy różnorodne tła – od klimatycznych uliczek, przez nowoczesne budynki, po urokliwe kawiarnie. Każda sceneria może opowiedzieć inną historię, a światła miasta dodają zdjęciom wyjątkowego charakteru, zwłaszcza o zmierzchu. To propozycja dla tych, którzy lubią miejski styl i chcą zatrzymać w kadrze autentyczne emocje w dynamicznym otoczeniu."
     },
 
     {   id: 4, 
-        url: "../public/grupowe.jpg",
+        url: "/grupowe.jpg",
         title: "Sesje gupowe",
         description: "Sesja w mieście to świetny sposób na uchwycenie energii i rytmu życia! W miejskim krajobrazie znajdziemy różnorodne tła – od klimatycznych uliczek, przez nowoczesne budynki, po urokliwe kawiarnie. Każda sceneria może opowiedzieć inną historię, a światła miasta dodają zdjęciom wyjątkowego charakteru, zwłaszcza o zmierzchu. To propozycja dla tych, którzy lubią miejski styl i chcą zatrzymać w kadrze autentyczne emocje w dynamicznym otoczeniu."
     },
     {  id: 1,
-        url: "../public/miasto.jpg",
+        url: "/miasto.jpg",
         title: "Sesja w mieście",
         description: "Sesja w mieście to świetny sposób na uchwycenie energii i rytmu życia! W miejskim krajobrazie znajdziemy różnorodne tła – od klimatycznych uliczek, przez nowoczesne budynki, po urokliwe kawiarnie. Każda sceneria może opowiedzieć inną historię, a światła miasta dodają zdjęciom wyjątkowego charakteru, zwłaszcza o zmierzchu. To propozycja dla tych, którzy lubią miejski styl i chcą zatrzymać w kadrze autentyczne emocje w dynamicznym otoczeniu."
      },
      {   id: 2, 
-         url: "../public/przyroda.jpg",
+         url: "/przyroda.jpg",
          title: "Sesja na przyrodzie",
          description: "Sesja w mieście to świetny sposób na uchwycenie energii i rytmu życia! W miejskim krajobrazie znajdziemy różnorodne tła – od klimatycznych uliczek, przez nowoczesne budynki, po urokliwe kawiarnie. Każda sceneria może opowiedzieć inną historię, a światła miasta dodają zdjęciom wyjątkowego charakteru, zwłaszcza o zmierzchu. To propozycja dla tych, którzy lubią miejski styl i chcą zatrzymać w kadrze autentyczne emocje w dynamicznym otoczeniu."
      },
      {   id: 3,
-         url: "../public/studio.jpg",
+         url: "/studio.jpg",
          title: "Sesja w studio",
          description: "Sesja w mieście to świetny sposób na uchwycenie energii i rytmu życia! W miejskim krajobrazie znajdziemy różnorodne tła – od klimatycznych uliczek, przez nowoczesne budynki, po urokliwe kawiarnie. Każda sceneria może opowiedzieć inną historię, a światła miasta dodają zdjęciom wyjątkowego charakteru, zwłaszcza o zmierzchu. To propozycja dla tych, którzy lubią miejski styl i chcą zatrzymać w kadrze autentyczne emocje w dynamicznym otoczeniu."
      },
  
      {   id: 4, 
-         url: "../public/grupowe.jpg",
+         url: "/grupowe.jpg",
          title: "Sesje gupowe",
          description: "Sesja w mieście to świetny sposób na uchwycenie energii i rytmu życia! W miejskim krajobrazie znajdziemy różnorodne tła – od klimatycznych uliczek, przez nowoczesne budynki, po urokliwe kawiarnie. Każda sceneria może opowiedzieć inną historię, a światła miasta dodają zdjęciom wyjątkowego charakteru, zwłaszcza o zmierzchu. To propozycja dla tych, którzy lubią miejski styl i chcą zatrzymać w kadrze autentyczne emocje w dynamicznym otoczeniu."
      },
